2c653af2828c521abdd2387c871e2f98938a2ef2
poojakondoju <kondojupoojitha612@gmail.com>
2021-08-21 22:13:26 +0530

    Added: welcome to EmployeeDailywage

---
+++ src/com/employeewage/EmployeeDaiywage.java
@@ -0,0 +1,10 @@
+package com.employeewage;
+
+public class EmployeeDaiywage {
+	public static void main( String[] args )
+    {
+        System.out.println( "Welcome to LineComparison Computation" );
+    }
+
+
+}
