93695c1086a7aa10749db72193f751d17993cfb5
poojakondoju <kondojupoojitha612@gmail.com>
2021-08-21 22:23:36 +0530

    Added: Employee is present or Absent

--- src/com/employeewage/EmployeeDaiywage.java
+++ src/com/employeewage/EmployeeDaiywage.java
@@ -1,10 +1,23 @@
 package com.employeewage;
 
 public class EmployeeDaiywage {
-	public static void main( String[] args )
-    {
-        System.out.println( "Welcome to LineComparison Computation" );
-    }
+		public class EmployeeWageComputation
+		{
+		    public static void main(String args[])
+		    {
+		        final int FULL_TIME = 1;
+		        int empType = (int) (Math.random() * 100) % 2;
+		        if (empType == FULL_TIME)
+		        {
+		            System.out.println("Employee is Present");
+
+		        } else
+		        {
+		            System.out.println("Employee is   Absent");
+
+		        }
+		    }
+		}
 
 
-}
+	}
